971598f51b469805bdbd4dd9a54cc06f2e689f9e
Shane0018 <shanegroup1@gmail.com>
2020-12-10 18:49:46 -0500

    counter loops

---
+++ exercise6.rb
@@ -0,0 +1,27 @@
+my_name = "Miss Shane"
+puts "please enter your name"
+user_name = gets.chomp
+if user_name == my_name
+    puts "We have the same name!"
+end 
+
+# if user_name > 10
+#     puts "hi #{user_name}"
+# elsif user_name < 10 
+#     puts "hello #{user_name}"
+# else user_name == 10 
+#     puts "hey #{user_name}"
+# end 
+
+secret_number = 6
+puts "try to guess the secret number"
+user_number = gets.chomp.to_i
+
+if user_number == secret_number
+    puts "You win!"
+elsif (user_number - secret_number == 1) || (secret_number - user_number == 1)
+    puts "So close!"
+else 
+    puts "Try again"
+end 
+
---
+++ exercise7.rb
@@ -0,0 +1,13 @@
+counter = 1
+
+while counter < 4
+    puts "counter currently at #{counter}"
+    counter += 1 
+end
+
+counter = 3
+
+until counter == 0
+    puts "counter currently at #{counter}"
+    counter -=1
+end 
