9c263e74d86c016367ccb74afaaaae8e8686f4c3
Shane0018 <shanegroup1@gmail.com>
2020-12-10 15:09:11 -0500

    claculating year of birth

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,7 @@
 puts "What is your name?" 
 user_name = gets.chomp
 puts "Hello #{user_name}"
+puts "how old are you?"
+user_age = gets.chomp
+d_o_b = 2020 - user_age.to_i
+puts "you were born in the year #{d_o_b}"
